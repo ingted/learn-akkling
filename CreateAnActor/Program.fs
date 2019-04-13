874bce073952837b1a7d8299bc3404015b98dad6
Larry Jones <larry.jones@reveal-energy.com>
2019-04-13 12:47:02 -0500

    (BROKE) Creating an actor explicitly

---
+++ CreateAnActor/Program.fs
@@ -0,0 +1,25 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open Akkling
+
+[<EntryPoint>]
+let main argv =
+    use system = System.create "my-system" (Configuration.defaultConfig())
+    let helloRef = spawn system "hello-actor" <| props(fun context ->
+        let rec loop () = actor {
+            let! msg = context.Receive ()
+            match msg with
+            | "stop" -> return Stop // effect - stops current actor
+            | "unhandle" -> return Unhandled // effect - marks message as unhandled
+            | x -> 
+                printfn "%s" x
+                return! loop ()
+        }
+        loop ())
+   
+    helloRef <! "Tom"
+    helloRef <! "Dick"
+    
+    system.Terminate().Wait()
+    
+    0 // return an integer exit code
