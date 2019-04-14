cde6ae7b78d4407d794206bd9d224a6f8b08afdf
Larry Jones <larry.jones@reveal-energy.com>
2019-04-14 11:37:47 -0500

    Demonstrate common actor behaviors

---
+++ Behaviors/Program.fs
@@ -0,0 +1,25 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System.Threading
+open Akkling
+
+[<EntryPoint>]
+let main argv =
+    use system = System.create "my-system" (Configuration.defaultConfig())
+    
+    // The ignore behavior "eats" all messages sent to it.
+    let blackhole = spawnAnonymous system <| props Behaviors.ignore
+    blackhole <! "Tom"
+    
+    // The printf behavior prints all incoming messages to standard out using the specified format.
+    let printer = spawnAnonymous system <| props (Behaviors.printf "%s")
+    printer <! "Dick"
+    
+    // The echo behavior simply sends any message received back to the sender.
+    // I'm uncertain how to test this behavior. :(
+    let echo = spawnAnonymous system <| props Behaviors.echo
+                                               
+    // Wait to finish handling all the messages
+    Thread.Sleep(100)
+    
+    0 // return an integer exit code
