e0e5a756f77f8017657b3f2868ce83baa16bba5d
Larry Jones <larry.jones@reveal-energy.com>
2019-04-14 11:06:50 -0500

    Demonstrate actorOf with behavior

---
+++ UseActorOf/Program.fs
@@ -0,0 +1,33 @@
+﻿// Create an actor using the `actorOf` shortcut
+
+open System.Threading
+open Akkling
+
+let helloBehavior = function
+    | "stop" -> Stop
+    | "unhandle" -> Unhandled
+    | x ->
+        printfn "%s" x
+        Ignore
+    
+
+[<EntryPoint>]
+let main argv =
+    use system = System.create "my-system" (Configuration.defaultConfig())
+    let helloRef = spawn system "hello-greeter" <| props (actorOf helloBehavior)
+    
+    // Three "typical messages"
+    helloRef <! "Tom"
+    helloRef <! "Dick"
+    helloRef <! "Harry"
+    
+    // An "unhandled" message
+    helloRef <! "unhandle"
+    
+    // Finally, stop the actor
+    helloRef <! "stop"
+    
+    // Wait to finish handling all the messages
+    Thread.Sleep(100)
+    
+    0 // return an integer exit code
